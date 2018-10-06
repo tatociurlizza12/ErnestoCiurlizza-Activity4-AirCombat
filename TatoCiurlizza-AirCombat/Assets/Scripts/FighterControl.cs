4be67c791250c7d66d147fab67da922414d68f77
Ernesto Ciurlizza <tciurlizzas@gmail.com>
2018-10-06 16:43:46 -0500

    Hola
    Signed-off-by: Ernesto Ciurlizza <tciurlizzas@gmail.com>

---
+++ TatoCiurlizza-AirCombat/Assets/Scripts/FighterControl.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterControl : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Right
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(Vector3.left);
+        }
+
+        //Motion Left
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(Vector3.right);
+        }
+
+        //Up
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(Vector3.up);
+        }
+
+        //Down
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(Vector3.down);
+        } 
+
+        //Space key for shooting
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Creamos un nuevo gameobject Missile a partir de un prefab (planos de Construccion)
+            GameObject newmisil = GameObject.Instantiate(Resources.Load("Bullets/misil") as GameObject);
+
+            //Buscamos el Pivote del disparo
+            GameObject ShootPivot = GameObject.Find("Avion/ShootPivot");
+
+            //Hacemos que el misil sea hijo de Shoot pivot
+            newmisil.transform.SetParent(ShootPivot.transform);
+
+            //Movemos el misil hacia donde esta el shoot pvot, es decir, debajo del ala
+            newmisil.transform.localPosition = Vector3.zero;
+
+            //Hacemos que los misiles ya no sean hijos del avion
+            newmisil.transform.SetParent(null);
+        }
+
+    } //End Update
+}
---
+++ TatoCiurlizza-AirCombat/Assets/Scripts/Misil.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Misil : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+        //Movimiento hacia adelante
+        this.transform.Translate(Vector3.forward * speed);
+    }
+
+    // End Update
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Colision dectectada");
+
+        //Buscamos un gameobject llamado Enemy
+        GameObject enemy = collision.collider.gameObject;
+
+        //Eliminamos el gameobject encontrado
+        GameObject.Destroy(enemy);
+
+        //Eliminamos tambin el misil
+        GameObject misil = this.gameObject;
+        GameObject.Destroy(misil);
+    }
+     
+
+}
